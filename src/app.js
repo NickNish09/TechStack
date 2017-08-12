f18e3ff922cd0bfcbc2dda6ba8e8cbdc79af2a43
NickNish09 <nicholasmarques@cjr.org.br>
2017-08-11 22:40:07 -0300

    Feito Reducer das bibliotecas, arquivo json com dados incluido

--- src/app.js
+++ src/app.js
@@ -3,12 +3,15 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
 	return (
 		<Provider store={createStore(reducers)}>	
 			<View>
-				<Text> Oi </Text>
+				<Header headerText="Tech Stack" />
+				<LibraryList />
 			</View>
 		</Provider>
 	);
